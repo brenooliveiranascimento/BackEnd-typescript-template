73750ae05099fb6a432bb05e02cb0e3636dfeb5b
Breno Nascimento <satsukebare23@gmail.com>
2022-11-02 16:17:09 -0300

    add create new book and middleware validation bookdata

--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -9,6 +9,21 @@ class BooksController {
     const books = await this.bookService.getAll();
     res.status(statusCodes.OK).json({message: books, status: 'Sucesso!'});
   };
+
+  public getById = async (req: Request, res: Response) => {
+    const id = parseInt(req.params.id);
+    const book = await this.bookService.getById(id);
+    if(!book) return res.status(statusCodes.NOT_FOUND).json({ message: 'Book not found!' });
+    
+    res.status(statusCodes.OK).json(book);
+  };
+
+  public create = async (req: Request, res: Response) => {
+    const book = req.body;
+    const createBook = await this.bookService.create(book);
+    console.log(createBook)
+    res.status(statusCodes.OK).json(createBook);
+  }
 }
 
 export default BooksController;
--- src/services/books.service.ts
+++ src/services/books.service.ts
@@ -7,6 +7,20 @@ class BookService {
     const books = await BookModel.findAll();
     return books;
   }
+
+  public async getById(id: number): Promise<Book> {
+    const book = await BookModel.findByPk(id);
+    return book as Book;
+  }
+
+  public async create(book: Book) {
+    const { author, isbn, price, title } = book;
+    const createBook = await BookModel.create({
+      author, isbn, price, title
+    })
+    return createBook
+  }
+
 }
 
 export default BookService;
